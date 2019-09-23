618f128a6e8e2b65944adb61f2bd22d3abe344bf
Maria_Kirdun <manya.gymil@gmail.com>
2019-09-23 16:11:12 +0300

    add empty click function to radio buttons

--- app/src/main/java/com/manya/simplestencryptionalgorithms/MainActivity.kt
+++ app/src/main/java/com/manya/simplestencryptionalgorithms/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.manya.simplestencryptionalgorithms
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +18,53 @@ class MainActivity : AppCompatActivity() {
         super.onResume()
         val algorithm = RailwayFenceEncryption("cryptogram",4)
         Log.d("defy", "${algorithm.encrypt()} \t")
-        val algorithmD = RailwayFenceEncryption("ctaromygpr",4)
-        Log.d("defy", "${algorithmD.decrypt()} \t")
 
     }
+
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked =  view.isChecked
+
+            when (view.id) {
+                R.id.railwayFenceRadioButton ->
+                /**
+                 * railwayFence
+                 */
+                    if (checked) {
+
+                    }
+                R.id.columnMethodRadioButton ->
+                /**
+                 * columnMethod
+                 */
+                    if (checked) {
+
+                    }
+                R.id.vigenereDirectRadioButton ->
+                /**
+                 * vigenereDirectRadioButton
+                 */
+                    if (checked) {
+
+                    }
+                R.id.vigenereProgressiveRadioButton ->
+                /**
+                 * vigenereProgressiveRadioButton
+                 */
+                    if (checked) {
+
+                    }
+                R.id.vigenereSelfGeneratedRadioButton ->
+                /**
+                 * vigenereSelfGeneratedRadioButton
+                 */
+                    if (checked) {
+
+                    }
+                else -> Toast.makeText(this,"Please choose algorithm", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
 }
