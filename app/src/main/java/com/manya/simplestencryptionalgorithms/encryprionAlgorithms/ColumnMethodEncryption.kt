07b6f8a3e8c7234ad8e2c03e9f8d6e757b458689
Maria_Kirdun <manya.gymil@gmail.com>
2019-09-24 18:36:47 +0300

    Add encryption algorithm for column method  Encrypted correct when key is not repeated

--- app/src/main/java/com/manya/simplestencryptionalgorithms/encryprionAlgorithms/ColumnMethodEncryption.kt
+++ app/src/main/java/com/manya/simplestencryptionalgorithms/encryprionAlgorithms/ColumnMethodEncryption.kt
@@ -3,8 +3,27 @@ package com.manya.simplestencryptionalgorithms.encryprionAlgorithms
 class ColumnMethodEncryption : Encryption {
 
     override fun encrypt(message: String, key: String): String {
-        sortAlphabetically(key)
-        return sortAlphabetically(key).contentToString()
+        val matrix = createMatrix(message,key)
+        val result = CharArray(matrix.get(0).size * matrix.size) {' '}
+
+        val ascii = getASCIINumbers(key)
+        val sortAscii = ascii.sortedArray()
+
+        var iterator = 0
+        for ( row in matrix.indices) {
+
+            for (col in matrix[row].indices) {
+                val i = sortAscii.indexOf(ascii[col])
+                if (i < result.size && i >= 0) {
+                    result[ row + i + iterator] = matrix[row][col]
+                }
+//                if ((row + 1) >= matrix.size){
+//                    sortAscii[i] = 0
+//                }
+            }
+            iterator += key.length - 1
+        }
+        return String(result).replace(" ", "")
 
     }
 
@@ -12,19 +31,22 @@ class ColumnMethodEncryption : Encryption {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun createMatrix(message: String, key: String) : Array<Array<String>>{
+    private fun createMatrix(message: String, key: String) : Array<Array<Char>>{
         val col = key.length
         var row = message.length / col
         if (message.length % col != 0) {
             row++
         }
-        return Array(row) {Array(col){" "}}
+        var i = 0;
+        return Array(row) {Array(col){if (i < message.length) message[i++] else ' '}}
     }
 
-    private fun sortAlphabetically(key : String) : Array<Int>{
+    private fun getASCIINumbers(key : String) : Array<Int>{
         val array : Array<Int> = Array(key.length){0}
-        for ((i, char) in key.indices.withIndex()) {
-            array[i] = char
+        var i = 0
+        for (char in key) {
+            array[i] = char.toInt()
+            i++
         }
         return array
     }
