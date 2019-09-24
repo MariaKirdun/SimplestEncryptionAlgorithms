ed125389630efc0592e2a14e7a864582bc0e72d7
Maria_Kirdun <manya.gymil@gmail.com>
2019-09-24 15:14:26 +0300

    Syntax correction

--- app/src/main/java/com/manya/simplestencryptionalgorithms/encryprionAlgorithms/ColumnMethodEncryption.kt
+++ app/src/main/java/com/manya/simplestencryptionalgorithms/encryprionAlgorithms/ColumnMethodEncryption.kt
@@ -3,11 +3,30 @@ package com.manya.simplestencryptionalgorithms.encryprionAlgorithms
 class ColumnMethodEncryption : Encryption {
 
     override fun encrypt(message: String, key: String): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        sortAlphabetically(key)
+        return sortAlphabetically(key).contentToString()
+
     }
 
     override fun decrypt(message: String, key: String): String {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun createMatrix(message: String, key: String) : Array<Array<String>>{
+        val col = key.length
+        var row = message.length / col
+        if (message.length % col != 0) {
+            row++
+        }
+        return Array(row) {Array(col){" "}}
+    }
+
+    private fun sortAlphabetically(key : String) : Array<Int>{
+        val array : Array<Int> = Array(key.length){0}
+        for ((i, char) in key.indices.withIndex()) {
+            array[i] = char
+        }
+        return array
     }
 
 }
